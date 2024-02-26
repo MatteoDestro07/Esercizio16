42f70136fc04dd03b3a12e94ab86e6f9cce8c3c8
Matteo Destro <dorianodx@gmail.com>
2024-02-26 19:30:10 +0100

    Aggiunta del menu di gioco e visualizza risultati

--- Esercizio16/Program.cs
+++ Esercizio16/Program.cs
@@ -15,164 +15,223 @@ namespace Esercizio16
     {
         static void Main(string[] args)
         {
-            int pedG = 0, pedR = 0;
             char C = ' ';
-            int posI = 7, posOLD = 7;
-            int N = 0;
-            int Turno = 1;
-            bool vG = false;
-            bool vR = false;
-            bool pareggio = false;
-            int R = 19;
-            int[,] M = new int[8, 8];
-            int j = 0;
-            int posMax = 35;
-            int posMin = 7;
-            int i = 0;
-            int cont = 0;
-            int contP = 0;
+            int scelta = 0;
+            int contVG = 0;
+            int contVR = 0;
+            int contPareggio = 0;
 
             /*
              * Turno 1 ==> ROSSO
              * Turno 2 ==> GIALLO
              */
 
-            // Creazione delle MATRICE di Gioco a 0
-            caricaMatrice(M);
-
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.White;
-
-            // Creo la Struttura (Campo da Gioco)
-            Struttura();
-
-            // Gestione del Punteggio
-            Punteggio(Turno, pedG, pedR);
-
-            // Gestione della Pedina
-            Pedina(posI, ref posOLD, N, Turno, ref R, ref M, posMin, posMax, ref i);
-
             do
             {
-                // Controllo la "pressione" di un Tasto / Carattere
-                if (Console.KeyAvailable)
+                scelta = disegnaMenu();
+
+                switch(scelta)
                 {
-                    C = Convert.ToChar(Console.ReadKey(false).Key);
-                    N = Convert.ToInt32(C);
+                    case 1:
+                        int pedG = 0, pedR = 0;
+                        int posI = 7, posOLD = 7;
+                        int N = 0;
+                        int Turno = 1;
+                        bool vG = false;
+                        bool vR = false;
+                        bool pareggio = false;
+                        int R = 19;
+                        int[,] M = new int[8, 8];
+                        int j = 0;
+                        int posMax = 35;
+                        int posMin = 7;
+                        int i = 0;
+                        int cont = 0;
+                        int contP = 0;
 
-                    switch (posI)
-                    {
-                        case 7: j = 0; break;
-                        case 11: j = 1; break;
-                        case 15: j = 2; break;
-                        case 19: j = 3; break;
-                        case 23: j = 4; break;
-                        case 27: j = 5; break;
-                        case 31: j = 6; break;
-                        case 35: j = 7; break;
-                    }
+                        // Creazione delle MATRICE di Gioco a 0
+                        caricaMatrice(M);
 
-                    switch (N)
-                    {
-                        case 37:
-                            // Freccia SX
-                            if (posI > posMin)
-                                posI -= 4;
-                            break;
-                        case 39:
-                            // Freccia DX
-                            if (posI < posMax)
-                                posI += 4;
-                            break;
-                        case 40:
-                            // Freccia DOWN
-                            if (Turno == 1)
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        // Creo la Struttura (Campo da Gioco)
+                        Struttura();
+
+                        // Gestione del Punteggio
+                        Punteggio(Turno, pedG, pedR);
+
+                        // Gestione della Pedina
+                        Pedina(posI, ref posOLD, N, Turno, ref R, ref M, posMin, posMax, ref i);
+
+                        do
+                        {
+                            // Controllo la "pressione" di un Tasto / Carattere
+                            if (Console.KeyAvailable)
                             {
-                                pedR++;
-                                Turno++;
+                                C = Convert.ToChar(Console.ReadKey(false).Key);
+                                N = Convert.ToInt32(C);
+
+                                switch (posI)
+                                {
+                                    case 7: j = 0; break;
+                                    case 11: j = 1; break;
+                                    case 15: j = 2; break;
+                                    case 19: j = 3; break;
+                                    case 23: j = 4; break;
+                                    case 27: j = 5; break;
+                                    case 31: j = 6; break;
+                                    case 35: j = 7; break;
+                                }
+
+                                switch (N)
+                                {
+                                    case 37:
+                                        // Freccia SX
+                                        if (posI > posMin)
+                                            posI -= 4;
+                                        break;
+                                    case 39:
+                                        // Freccia DX
+                                        if (posI < posMax)
+                                            posI += 4;
+                                        break;
+                                    case 40:
+                                        // Freccia DOWN
+                                        if (Turno == 1)
+                                        {
+                                            pedR++;
+                                            Turno++;
+                                        }
+                                        else
+                                        {
+                                            pedG++;
+                                            Turno--;
+                                        }
+
+                                        // Controllo se la colonna è piena
+                                        while ((M[0, j] == 1 || M[0, j] == 2) && posI == posMax && !pareggio)
+                                        {
+                                            posOLD = posI;
+                                            posI -= 4;
+                                            posMax -= 4;
+                                            Console.SetCursorPosition(posOLD, 1);
+                                            Console.Write("    ");
+                                            j--;
+                                        }
+
+                                        while ((M[0, j] == 1 || M[0, j] == 2) && posI > posMin && posI < posMax && !pareggio)
+                                        {
+                                            posOLD = posI;
+                                            Console.SetCursorPosition(posOLD, 1);
+                                            Console.Write("     ");
+                                            posI += 4;
+                                            j++;
+                                        }
+
+                                        while ((M[0, j] == 1 || M[0, j] == 2) && posI == posMin && !pareggio)
+                                        {
+                                            posOLD = posI;
+                                            Console.SetCursorPosition(posOLD, 1);
+                                            Console.Write("     ");
+                                            posI += 4;
+                                            posMin += 4;
+                                            j++;
+                                        }
+
+                                        // Gestione del Punteggio
+                                        Punteggio(Turno, pedG, pedR);
+
+                                        break;
+
+                                }
+
+                                // Gestione della Pedina
+                                Pedina(posI, ref posOLD, N, Turno, ref R, ref M, posMin, posMax, ref i);
+                                contP++;
+
+                                // Controllo Nuova Pedina - Forza 4
+                                ControlloColonna(M, Turno, ref vG, ref vR, j, i, ref cont);
+                                if (cont != 4)
+                                    ControlloDP(M, Turno, ref vG, ref vR, j, i, ref cont);
+                                if (cont != 4)
+                                    ControlloRiga(M, Turno, ref vG, ref vR, i, ref cont);
+                                if (cont != 4)
+                                    ControlloDS(M, Turno, ref vG, ref vR, j, i, ref cont);
+
+                                if (cont == 4)
+                                {
+                                    if (Turno == 2)
+                                        vR = true;
+                                    else
+                                        vG = true;
+                                }
+                                else if (contP == 64)
+                                    pareggio = true;
                             }
-                            else
-                            {
-                                pedG++;
-                                Turno--;
-                            }
 
-                            // Controllo se la colonna è piena
-                            while ((M[0, j] == 1 || M[0, j] == 2) && posI == posMax && !pareggio)
-                            {
-                                posOLD = posI;
-                                posI -= 4;
-                                posMax -= 4;
-                                Console.SetCursorPosition(posOLD, 1);
-                                Console.Write("    ");
-                                j--;
-                            }
+                        }
+                        while ((C != 81) && !vG && !vR && !pareggio); // a Scelta !!!
 
-                            while ((M[0, j] == 1 || M[0, j] == 2) && posI > posMin && posI < posMax && !pareggio)
-                            {
-                                posOLD = posI;
-                                Console.SetCursorPosition(posOLD, 1);
-                                Console.Write("     ");
-                                posI += 4;
-                                j++;
-                            }
+                        Console.SetCursorPosition(0, 25);
+                        if (vG)
+                        {
+                            Console.WriteLine("Vittoria del giallo");
+                            contVG++;
+                        }
+                        else if (vR)
+                        {
+                            Console.WriteLine("Vittoria del rosso");
+                            contVR++;
+                        }
+                        else if (pareggio)
+                        {
+                            Console.WriteLine("Pareggio");
+                            contPareggio++;
+                        }
+                        attesa();
+                        break;
 
-                            while ((M[0, j] == 1 || M[0, j] == 2) && posI == posMin && !pareggio)
-                            {
-                                posOLD = posI;
-                                Console.SetCursorPosition(posOLD, 1);
-                                Console.Write("     ");
-                                posI += 4;
-                                posMin += 4;
-                                j++;
-                            }
-
-                            // Gestione del Punteggio
-                            Punteggio(Turno, pedG, pedR);
-                                
-                            break;
-
-                    }
-
-                    // Gestione della Pedina
-                    Pedina(posI, ref posOLD, N, Turno, ref R, ref M, posMin, posMax, ref i);
-                    contP++;
-
-                    // Controllo Nuova Pedina - Forza 4
-                    ControlloColonna(M, Turno, ref vG, ref vR, j, i, ref cont);
-                    if (cont != 4)
-                        ControlloDP(M, Turno, ref vG, ref vR, j, i, ref cont);
-                    if (cont != 4) 
-                        ControlloRiga(M, Turno, ref vG, ref vR, i, ref cont);
-                    if(cont != 4)
-                        ControlloDS(M, Turno, ref vG, ref vR, j, i, ref cont);
-
-                    if (cont == 4)
-                    {
-                        if (Turno == 2)
-                            vR = true;
+                    case 2:
+                        if (contVG > 0 || contVR > 0 || contPareggio > 0)
+                            visualizzaRisultati(contVG, contVR, contPareggio);
                         else
-                            vG = true;
-                    }
-                    else if (contP==64)
-                        pareggio = true;
-                    
+                            Console.WriteLine("Gioca almeno una partita per vedere i risultati");
+                        break;
                 }
-
             }
-            while ((C != 81) && !vG && !vR && !pareggio); // a Scelta !!!
-
-            Console.SetCursorPosition(0, 25);
-            if (vG)
-                Console.WriteLine("Vittoria del giallo");
-            else if (vR)
-                Console.WriteLine("Vittoria del rosso");
-            else if (pareggio)
-                Console.WriteLine("Pareggio");
-
-            attesa();
+            while (scelta != 0);
+            
 
         } // FINE MAIN
+
+        public static void visualizzaRisultati(int VG, int VR, int Par)
+        {
+            Console.Clear();
+            Console.WriteLine("----RISULTATI----");
+            Console.WriteLine($"Vittorie Rosso: {VR}");
+            Console.WriteLine($"Vittorie Giallo: {VG}");
+            Console.WriteLine($"Pareggi :{Par}");
+
+            attesa();
+        }
+
+        public static int disegnaMenu()
+        {
+            int sc = 0;
+
+            Console.Clear();
+            Console.WriteLine("----FORZA 4----");
+            Console.WriteLine("1. Gioca");
+            Console.WriteLine("2. Visualizza Risultati");
+            Console.WriteLine("0. ESCI");
+
+            Console.WriteLine();
+            Console.WriteLine("Inserisci la tua scelta == >");
+            sc = Convert.ToInt32(Console.ReadLine());
+
+            return sc;
+        }
 
         public static void ControlloDS(int[,] M, int T, ref bool vG, ref bool vR, int j, int i, ref int cont)
         {
@@ -184,14 +243,12 @@ namespace Esercizio16
             else
                 x = 2;
 
-            //Decremento i e j fino a che una valga zero
             while (i > 0 && j < 7)
             {
                 j++;
                 i--;
             }
 
-            //Ciclo fino a quando i o j vale 8 o cont sia uguale a 4 
             while (i < 7 && j > 0 && cont < 4)
             {
                 if (x == M[i, j])
@@ -201,12 +258,8 @@ namespace Esercizio16
                     cont = 1;
                 }
 
-                //Aumento di 1 sia i che j per rimanere sulla stessa diagonale
                 i++;
                 j--;
-
-                Console.SetCursorPosition(0, 25);
-                Console.Write(cont);
             }
         }
 
@@ -241,14 +294,12 @@ namespace Esercizio16
             else
                 x = 2;
 
-            //Decremento i e j fino a che una valga zero
             while (i > 0 && j > 0)
             {
                 j--;
                 i--;
             }
 
-            //Ciclo fino a quando i o j vale 8 o cont sia uguale a 4 
             while (i < 8 && j < 8)
             {
                 if (x == M[i, j])
@@ -258,7 +309,6 @@ namespace Esercizio16
                     cont = 0;
                 }
 
-                //Aumento di 1 sia i che j per rimanere sulla stessa diagonale
                 i++;
                 j++;
             }
@@ -282,17 +332,6 @@ namespace Esercizio16
                     x = M[i, j];
                 }
                 i++;
-            }
-        }
-
-        static void StampaMatrice(int[,] M, int Rm, int Cm)
-        {
-            for (int i = 0; i < Rm; i++) //gestisco le righe
-            {
-                for (int j = 0; j < Cm; j++)
-                    Console.Write(M[i, j]);
-
-                Console.WriteLine();
             }
         }
 
